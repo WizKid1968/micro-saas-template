b114207606c345eba0cb98e08a3a2771aeee73eb
wizkid68 <rllef1968@gmail.com>
2024-08-25 02:34:36 -0400

    Remove unnecessary Login component from main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,12 @@ import LandingPage from "@/components/LandingPage";
 import dynamic from 'next/dynamic';
 
 const DynamicDashboard = dynamic(() => import('../components/Dashboard'), { ssr: false });
-const DynamicLogin = dynamic(() => import('../components/Login'), { ssr: false });
 
 export default function Home() {
   return (
     <>
       <LandingPage />
       <DynamicDashboard />
-      <DynamicLogin />
     </>
   );
 }
